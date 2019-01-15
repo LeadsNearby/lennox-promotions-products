b4bff3dc9cc7b9f0a4300fcd16526f1459a5f1f0
Andrew Gillingham <andrew@leadsnearby.com>
2019-01-15 09:21:35 -0500

    wip

--- assets/js/products-helper.js
+++ assets/js/products-helper.js
@@ -1,36 +1,36 @@
 jQuery(document).ready(function($) {
-	// var ajaxurl = '/wp-admin/admin-ajax.php';
+  // var ajaxurl = '/wp-admin/admin-ajax.php';
 
-	$('.lennox-category .lennox-cat-vm-button').click(function() {
-		var lennoxCatID = $(this).attr('lennox-category-id');
+  $('.lennox-category .lennox-cat-vm-button').click(function() {
+    var lennoxCatID = $(this).attr('lennox-category-id');
 
-		window.location.assign('?cat='+lennoxCatID);
+    window.location.assign('?cat=' + lennoxCatID);
 
-	// 	$.ajax({
-	// 		type:'POST',
-	// 		url: ajaxurl,
-	// 		data: {
-	// 			action: 'lennox_products_ajaxhandler',
-	// 			'lnxType' : 'category',
-	// 			'lnxCatID' : lennoxCatID,
+    // 	$.ajax({
+    // 		type:'POST',
+    // 		url: ajaxurl,
+    // 		data: {
+    // 			action: 'lennox_products_ajaxhandler',
+    // 			'lnxType' : 'category',
+    // 			'lnxCatID' : lennoxCatID,
 
-	// 		},
-	// 		success: function(response) {
-	// 			// alert(response);
-	// 			$('.lennox-container').html(response);
-	// 		},
-	// 		error: function(errorThrown) {
-	// 			alert(errorThrown);
-	// 		}
-	// 	});
-	});
-	$('.lennox-product-vm').click(function() {
-		var lennoxProductID = $(this).attr('lennox-product-id');
-		// if (lennoxProductIDarray.length > 4) {
-		// 	var lennoxProductID = lennoxProductIDarray[2] + '-' + lennoxProductIDarray[3];
-		// } else {
-		// 	var lennoxProductID = lennoxProductIDarray[2];
-		// }
-		window.location.assign('?product='+lennoxProductID);
-	});
+    // 		},
+    // 		success: function(response) {
+    // 			// alert(response);
+    // 			$('.lennox-container').html(response);
+    // 		},
+    // 		error: function(errorThrown) {
+    // 			alert(errorThrown);
+    // 		}
+    // 	});
+  });
+  $('.lennox-product-vm').click(function() {
+    var lennoxProductID = $(this).attr('lennox-product-id');
+    // if (lennoxProductIDarray.length > 4) {
+    // 	var lennoxProductID = lennoxProductIDarray[2] + '-' + lennoxProductIDarray[3];
+    // } else {
+    // 	var lennoxProductID = lennoxProductIDarray[2];
+    // }
+    window.location.assign('?product=' + lennoxProductID);
+  });
 });
